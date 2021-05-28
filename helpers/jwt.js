5a88e2041d168b7c05cda63ef12a3a7d6477e121
matanbe4 <34807546+matanbe4@users.noreply.github.com>
2021-05-29 02:11:27 +0300

    Trying to fix heroku

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -4,19 +4,18 @@ function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
     
-    return expressJwt({
-        secret,
-        algorithms: ['HS256'],
-        isRevoked: isRevoked
-    }).unless({
-        path: [
-            `${api}/users/login`,
-            {url: /\/api\/v1\/products\/(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories\/(.*)/ , methods: ['GET', 'OPTIONS'] },
-            `${api}/users/register`
-        ]
-    })
+    return expressJwt({ secret, algorithms: ["HS256"], isRevoked: isRevoked, })
+.unless({ 
+path: [
+ { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+ { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+ { url: /\/api\/v1\/users(.*)/, methods: ["GET", "OPTIONS"] },
+ { url: /\/api\/v1\/orders(.*)/, methods: ["GET", "POST", "OPTIONS"] }, 
+`${api}/users/login`,
+ `${api}/users/signin`,
+ `${api}/users/register`, 
+],
+ });
 }
 
 async function isRevoked(req, payload, done) {
